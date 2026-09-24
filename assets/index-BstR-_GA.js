ef4f495aa300abd97c6119ae926d869c7e5a7844
gelson12 <gelson12@users.noreply.github.com>
2026-09-24 08:24:31 +0000

    deploy: dashboard from atlas-platform@0111846105e9be48afed119512a53507d88730ef gelson12/atlas-platform@0111846105e9be48afed119512a53507d88730ef

---
+++ assets/index-BstR-_GA.js
@@ -0,0 +1,1 @@
+import{aL as r}from"./index-DLt5f1EB.js";var o=r();export{o as r};
---
+++ assets/useFightEngineWake-Dz1E7YoX.js
@@ -0,0 +1,1 @@
+import{r as i,a1 as r}from"./index-DLt5f1EB.js";const o=300*1e3;function c(t=!0){i.useEffect(()=>{if(!t)return;const e=()=>{document.hidden||r.wakeEngine().catch(()=>{})};e();const n=window.setInterval(e,o);return document.addEventListener("visibilitychange",e),()=>{window.clearInterval(n),document.removeEventListener("visibilitychange",e)}},[t])}export{c as u};
